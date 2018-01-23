23cf2ad63d0dbe150d1715cb4874f6ee53597331
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2018-01-23 21:59:59 +0100

    Create post details page. images and comments

---
+++ client/components/Comments.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Comments extends Component {
+  renderComment(comment, i) {
+    return(
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    return(
+      <div className="comments">
+        {this.props.comments.map(this.renderComment)}
+      </div>
+    )
+  }
+}
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+import Photo from './Photo';
+import Comments from './Comments';
+
 export default class Single extends Component {
   render() {
-    return(
-      <p>This is single photo</p>
-    )
+    const { photoId } = this.props.params;
+    const { posts, comments } = this.props;
+    const i = posts.findIndex(
+      post => post.code === this.props.params.photoId
+    );
+    const post = posts[i];
+    const postComments = comments[photoId] || [];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments comments={postComments} />
+      </div>
+    );
   }
 }
