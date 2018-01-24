4a727143ce4e852c83b3754067daddfec5e34945
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2018-01-24 05:49:19 +0100

    Integrate redux dev tools

--- client/store.js
+++ client/store.js
@@ -14,7 +14,11 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enhancer = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancer);
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
